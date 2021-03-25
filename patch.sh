065f5d725fff39f1f77618c1c525d7a4b73412b1
Younghyun Cho <yhcho614@gmail.com>
2021-03-25 00:42:47 -0700

    Patch to CK soft module for searching with LD_LIBRARY_PATH and Python's sys.path

---
+++ patch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+output=$(ck  | grep 'Path to the default repo')
+ck_default_path=${output##* }
+additional_path="/module/soft/"
+ck_module_path=${ck_default_path}${additional_path}
+
+cp patch/soft_module.py $ck_module_path/module.py
