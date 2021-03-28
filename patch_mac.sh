bcc47d80f952d354614c8575c38220884587f678
Younghyun Cho <yhcho614@gmail.com>
2021-03-28 14:30:41 -0700

    Updated repo's meta data and missing setup file

---
+++ patch_mac.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+source ~/.bash_profile
+
+output=$(ck  | grep 'Path to the default repo')
+ck_default_path=${output##* }
+additional_path="/module/soft/"
+ck_module_path=${ck_default_path}${additional_path}
+
+cp patches/module:soft/module.py $ck_module_path/module.py
+cp patches/soft:lib.openmpi/customize.py ../ck-env/soft/lib.openmpi/customize.py
+cp patches/soft:lib.openmpi/meta.mac.json ../ck-env/soft/lib.openmpi/.cm/meta.json
