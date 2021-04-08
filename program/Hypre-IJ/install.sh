001ffe0e0d12b2e6877d6ff913e67adf72b20316
Younghyun Cho <yhcho614@gmail.com>
2021-04-08 02:18:41 -0700

    Added (updated) Hypre IJ tuning workflow

--- program/Hypre-IJ/install.sh
+++ program/Hypre-IJ/install.sh
@@ -1,11 +1,11 @@
-#/bin/bash
+#!/bin/bash
 
 rm -rf hypre
-git clone https://github.com/hypre-space/hypre.git
-cd hypre/src/
 
-./configure 
-#CC=$CCC CXX=$CCCPP FC=$FTN CFLAGS="-DTIMERUSEMPI"
+git clone https://github.com/hypre-space/hypre.git
+git checkout b3a4a76a5fc965b148b55ed8e9c342666b9c3c41
+cd hypre/src/
+./configure
 
 make
 cp ../../../hypre-driver/src/ij.c ./test/.
---
+++ program/Hypre-IJ/run_autotuner.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mpirun -n 1 python ../gptune_driver.py
